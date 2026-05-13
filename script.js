828154363bf73c9ec2e4a52dfe17c43f9bae87c6
Sandra <hi-sandra@hotmail.com>
2026-05-12 20:44:55 -0600

    fecha y hora

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function crearComentario() {
     let comentar = document.getElementById('comentar').value 
      if (comentar.trim() === "") return;
+
+    let ahora = new Date();
+    let fecha = ahora.toLocaleDateString();  
+    let hora = ahora.toLocaleTimeString(); 
