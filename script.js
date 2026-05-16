77ec32033f566f19e0f5848a305491e778b6b1eb
Sandra <hi-sandra@hotmail.com>
2026-05-16 17:39:29 -0600

    interactividad del color de los comentarios

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ function crearComentario() {
     let hora = ahora.toLocaleTimeString();
     
     let div = document.createElement("div");
+    div.style.background = '#f0f0f0';
+    div.style.borderRadius = '14px';
+    div.style.padding = '10px 16px';
+    div.style.fontSize = '14px';
+    div.style.color = '#444';
+    div.style.marginTop = '10px';
+
     div.innerHTML = `
         <p>
             ${comentar} <small>${fecha} ${hora}</small>
